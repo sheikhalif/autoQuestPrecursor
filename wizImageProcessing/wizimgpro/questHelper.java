140793dd7fe52a4c145af8410d16be0e9134e7ca
Sheikh Alif <salif30@stuy.edu>
2022-06-30 22:40:20 -0400

    found way to get pixel values quickly. now need to filter thru the values and pull out corner pixels so i can analyze their distances

--- wizImageProcessing/wizimgpro/questHelper.java
+++ wizImageProcessing/wizimgpro/questHelper.java
@@ -3,21 +3,40 @@ import java.lang.*;
 import java.awt.Color;
 import java.awt.Robot;
 import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
 public class questHelper{
   public static void main(String args[]){
     boolean active = true;
+    ArrayList<Integer> cornerX = new ArrayList<Integer>();
+    ArrayList<Integer> cornerY = new ArrayList<Integer>();
     try{
       Robot robot = new Robot();
+
       while (active){
         System.out.println("Running");
-        for (int i = 850; i < 1050; i++){
-          for (int x = 850; x < 1000; x++){
-            if (matchesYellow(robot.getPixelColor(i,x))){
-              System.out.println("Yellow found at: " + i + ", " + x);
+        Rectangle window = new Rectangle(850, 850, 250, 120);
+        BufferedImage yellowBox = robot.createScreenCapture(window);
+        for (int i = 0; i < 250; i++){
+          for (int x = 0; x < 120; x++){
+            Color currentColor = new Color(yellowBox.getRGB(i,x));
+            if (matchesYellow(currentColor)){
+              if (i > 0 && i < 249 && x > 0 && x < 119){
+                Color leftColor = new Color(yellowBox.getRGB(i-1, x));
+                Color rightColor = new Color(yellowBox.getRGB(i+1, x));
+                Color upColor = new Color(yellowBox.getRGB(i, x+1));
+                Color downColor = new Color(yellowBox.getRGB(i, x-1));
+                if (!(matchesYellow(leftColor)) || !(matchesYellow(rightColor)) || !(matchesYellow(upColor)) || !(matchesYellow(downColor))){
+                  cornerX.add(i);
+                  cornerY.add(x);
+                  System.out.println("Corner found at: " + (i+850) + ", " + (x+850));
+                }
+              }
             }
           }
         }
+        active = false;
       }
     }
     catch (AWTException e){
